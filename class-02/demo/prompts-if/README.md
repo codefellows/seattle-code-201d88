d69f9ccfa063103908f15abc846c0e8b2212f05a
Brian Sward <bsward@gmail.com>
2022-07-22 15:11:56 -0700

    all the extra crap i didn't need

--- class-02/demo/prompts-if/README.md
+++
@@ -1,4 +0,0 @@
-# About Me
-Assignment for Code 201 to create a personal profile page with a guessing game
-
-I like popsicles.
--- class-11/demo/audio-video/js/media.js
+++
@@ -1,7 +0,0 @@
-const levees = document.getElementById('levees');
-const randomizer = document.getElementById('randomizer');
-
-randomizer.addEventListener('click', function(){
-  levees.volume = Math.random();
-  levees.autoplay = true;
-});
